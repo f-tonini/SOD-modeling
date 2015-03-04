cda39bf4947cd9894de9b609eb2a946c74064267
f-tonini <ftonini84@gmail.com>
2015-03-04 10:58:08 -0500

    Added weather to SporeDisp functions

--- scripts/myfunctions_SOD.r
+++ scripts/myfunctions_SOD.r
@@ -323,7 +323,7 @@ radialDisp <- function(xyAOIPos, AOI_owin){
 }
 
 
-SporeDisp <- function(x, S, I, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponential', 'Gauss'), mean = NULL, sd = NULL, scale = NULL, gamma=NULL, 
+SporeDisp <- function(x, S, I, W=NULL, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponential', 'Gauss'), mean = NULL, sd = NULL, scale = NULL, gamma=NULL, 
                       wtype=c('Uniform', 'VM'), wdir=c('N','NE','E','SE','S','SW','W','NW'), kappa=NULL){
   
   out <- list()
@@ -393,9 +393,10 @@ SporeDisp <- function(x, S, I, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponent
           if (col0 < 1 | col0 > ncol(x)) next     ## outside the region ##
           
           if(S[row0, col0] > 0){  
-            currentPropS <- round(S[row0, col0] / (S[row0, col0] + I[row0, col0]), 2)
+            PropS <- round(S[row0, col0] / (S[row0, col0] + I[row0, col0]), 2)
             U <- runif(1)
-            if (U < currentPropS){
+            if(!is.null(W)) Prob <- PropS * W[row0, col0] else Prob <- PropS    #added weather
+            if (U < Prob){
               I[row0, col0] <- I[row0, col0] + 1 
               S[row0, col0] <- S[row0, col0] - 1
             } 
@@ -416,7 +417,7 @@ SporeDisp <- function(x, S, I, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponent
 }
 
 
-SporeDisp2 <- function(x, S, I, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponential', 'Gauss'), mean = NULL, sd = NULL, scale = NULL, gamma=NULL, 
+SporeDisp2 <- function(x, S, I, W=NULL, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponential', 'Gauss'), mean = NULL, sd = NULL, scale = NULL, gamma=NULL, 
                        wtype=c('Uniform', 'VM'), wdir=c('N','NE','E','SE','S','SW','W','NW'), kappa=NULL){
   
   out <- list()
@@ -488,12 +489,16 @@ SporeDisp2 <- function(x, S, I, rs, rtype=c('Cauchy', 'Cauchy Mixture', 'Exponen
           col0 <- col0[-which(bool==1)]
         }
         
+        #if all spores fall outside region continue loop to next cell
+        if (length(row0) == 0 | length(col0) == 0) next 
+        
         ##LOOP THROUGH ROW0 and COL0 TO ADD DISPERSED SPORES
         for(i in 1:length(row0)){
           if(S[row0[i], col0[i]] > 0){
-            currentPropS <- round(S[row0[i], col0[i]] / (S[row0[i], col0[i]] + I[row0[i], col0[i]]), 2)
+            PropS <- round(S[row0[i], col0[i]] / (S[row0[i], col0[i]] + I[row0[i], col0[i]]), 2)
             U <- runif(1)
-            if (U < currentPropS){
+            if(!is.null(W)) Prob <- PropS * W[row0[i], col0[i]] else Prob <- PropS    #added weather
+            if (U < Prob){    
               I[row0[i], col0[i]] <- I[row0[i], col0[i]] + 1 
               S[row0[i], col0[i]] <- S[row0[i], col0[i]] - 1
             }#ENF IF 
