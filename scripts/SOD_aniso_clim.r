5178389f2c8256ab35c5636ac04c31350decfda8
f-tonini <ftonini84@gmail.com>
2015-03-16 16:00:31 -0400

    Updated weather folder & added R_GRASS7 I/O

--- scripts/SOD_aniso_clim.r
+++ scripts/SOD_aniso_clim.r
@@ -1,6 +1,6 @@
-#--------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------
 # Name:         SOD_aniso_clim.r
-# Purpose:      Lattice-based simulation of the spread of pathogen P. ramorum over a heterogeneous landscape.
+# Purpose:      Lattice-based simulation of the climate-driven anisotropic spread of pathogen P. ramorum over a heterogeneous landscape.
 # Author:       Francesco Tonini
 # Email:        ftonini84@gmail.com
 # Created:      01/07/2015
@@ -10,21 +10,19 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 #install packages
-#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp"))
+#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp", "rgrass7"))
 
 #load packages
-library(raster)  		#Raster operation and I/O
-library(rgdal)	
-library(lubridate)
-library(CircStats)  #Von Mises distribution
-#library(spatstat)
-library(Rcpp)
+library(raster)  		#Raster operation and I/O. Depends R (≥ 2.15.0)
+library(rgdal)	    #Geospatial data abstraction library. Depends R (≥ 2.14.0)
+library(lubridate)  #Make dealing with dates a little easier. Depends R (≥ 3.0.0)
+library(CircStats)  #Circular Statistics - Von Mises distribution
+library(Rcpp)       #Seamless R and C++ Integration. Depends R (≥ 3.0.0)
+library(rgrass7)    #Interface Between GRASS 7 GIS and R. Depends R (≥ 2.12)
 
 
 ##Define the main working directory
-##Make sure to specify the appropriate path using either / or \\ to specify the path 
-#setwd("path_to_desired_folder")
-setwd("D:\\TangibleLandscape\\SOD-modeling")
+#setwd("path_to_main_folder")
 
 #Path to folders in which you want to save all your vector & raster files
 #fOutput <- 'output'
@@ -38,65 +36,54 @@ setwd("D:\\TangibleLandscape\\SOD-modeling")
 source('./scripts/myfunctions_SOD.r')
 sourceCpp("./scripts/myCppFunctions.cpp") #for C++ custom functions
 
+#WEATHER SUITABILITY: read and stack weather suitability raster BEFORE running the simulation
+lst <- dir('./layers/weather', pattern='\\.img$', full.names=T)
+Mlst <- lst[grep("_m", lst)]
+Clst <- lst[grep("_c", lst)]
 
-###Input simulation parameters:
-start <- 2004
-end <- 2006
-
-dd_start <- as.POSIXlt(as.Date(paste(start,'-01-01',sep='')))
-dd_end <- as.POSIXlt(as.Date(paste(end,'-12-31',sep='')))
-
-tstep <- as.character(seq(dd_start, dd_end, 'weeks'))
-months_msk <- paste('0', 1:9, sep='')
-
-#months <- c("January","February","March","April","May","June","July","August","September","October","November","December")
-#months_msk <- c("January","February","March","April","May","June","July","August","September")
-#tstep <- sapply(months, FUN=function(x) paste(x,start:end,sep=''))
-#tstep <- c(t(s))
-
-#WEATHER SUITABILITY:
-Mstack <- stack() #M = moisture; 
-Cstack <- stack() #C = temperature;
-
-for (yr in seq(start,end)){
-  
-  #moisture coeff
-  Mlst <- dir(paste('./layers/M/', yr, sep=''), pattern='\\.img$', full.names=T)
-  #ordered sequence
-  Mlst <- Mlst[match( paste('m',seq(1,52),sep=''),  sub("^([^.]*).*", "\\1", basename(Mlst)) )]
-  Mlst_rstck <- stack(Mlst)
-  
-  #temperature coeff
-  Clst <- dir(paste('./layers/C/', yr, sep=''), pattern='\\.img$', full.names=T)
-  Clst_rstck <- stack(Clst)
-  
-  Mstack <- stack(Mstack, Mlst_rstck)
-  Cstack <- stack(Cstack, Clst_rstck)
-  
-}
+Mstack <- stack(Mlst) #M = moisture; 
+Cstack <- stack(Clst) #C = temperature;
 
 
-set.seed(2000)
+###Input simulation parameters: #####
 
-##read initial raster (host index) with counts of max available "Susceptible" trees per cell
-##counts are integers [0, 100]
-Nmax_rast <- raster('./layers/HI_100m.img')
-Nmax <- Nmax_rast[]  #integer vector of Nmax
+##Input raster --> HOST INDEX
+Nmax_rast <- readRAST(commandArgs()[2]) #in the current version this reads raster in GRASS as a 'sp' R object
+Nmax_rast <- raster(Nmax_rast)  #transform 'sp' obj to 'raster' obj
+#Nmax_rast <- raster('./layers/HI_100m.img')
 
 #raster resolution
 res_win <- res(Nmax_rast)[1]
 
-#empty vector with counts of I (=infected trees)
-I_lst <- rep(0, length(Nmax))  #integer
-
 #clone Smax raster to I (=infected trees) raster and spores (=number of spores)
 I_rast <- Nmax_rast 
 
-#empty vector with counts of pathogen spores
-spores_lst <- rep(0, length(Nmax))  #integer
+
+##Start-End date: 
+start <- commandArgs()[3]
+end <- commandArgs()[4]
+#start <- 2004
+#end <- 2008
+
+if (start > end) stop('start date must precede end date!!')
+
+##Seasonality: Do you want the spread to be limited to certain months?
+ss <- commandArgs()[5]   #'ON' or 'OFF'
+#ss <- 'ON'
+
+if (ss == 'ON') months_msk <- paste('0', 1:9, sep='') #1=January 9=September
+
+
+
+set.seed(2000)
+
 
 ########################################################
-##SOURCES OF INFECTION:
+##INITIAL SOURCES OF INFECTION:
+#empty vector with counts of I (=infected trees)
+Nmax <- Nmax_rast[]  #integer vector of Nmax
+I_lst <- rep(0, length(Nmax))  #integer
+
 #initial sources of infection (integer between 0 and length(Smax))
 inf_src <- 2 #integer
 
@@ -108,6 +95,7 @@ inf_idx <- sample(which(Nmax > 0), size = inf_src)
 I_lst[inf_idx] <- sapply(Nmax[inf_idx], FUN=function(x) sample(1:x, size=1)) 
 ###############################################################################
 
+
 #Susceptibles = Nmax - Infected 
 S_lst <- Nmax - I_lst   #integer vector
 
@@ -117,9 +105,14 @@ infected <- matrix(I_lst, ncol=ncol(Nmax_rast), nrow=nrow(Nmax_rast), byrow=T)
 
 cnt <- 0 #time counter to access raster stacks
 
-##LOOP for each week
+#build time series:
+dd_start <- as.POSIXlt(as.Date(paste(start,'-01-01',sep='')))
+dd_end <- as.POSIXlt(as.Date(paste(end,'-12-31',sep='')))
+tstep <- as.character(seq(dd_start, dd_end, 'weeks'))
+
+
+##MAIN SIMULATION LOOP (weekly steps)
 for (tt in tstep){
-  
   
   if (tt == tstep[1]) {
     
@@ -136,86 +129,50 @@ for (tt in tstep){
     #I_rast[] <- ifelse(I_lst > 0, 1, 0) 
     #I_rast[] <- ifelse(I_lst > 0, 1, NA) 
     
-    breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
-    colors <- c("yellow","gold","orange","red")
-    plot(I_rast, breaks=breakpoints, col=colors, main=tt)
+    #breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
+    #colors <- c("yellow","gold","orange","red")
+    #plot(I_rast, breaks=breakpoints, col=colors, main=tt)
     
     #WRITE TO FILE:
+    I_rast <- as(I_rast, 'SpatialGridDataFrame')
+    writeRAST(I_rast, vname='Infected_0.img', overwrite=TRUE) #write to GRASS raster file
+    
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
     
   }else{
      
-    cnt <- cnt + 1
-    
-    #is current week time step within a spread month? spread month defined as 1-9 (Jan-Sep)
-    if (!any(substr(tt,6,7) %in% months_msk)) {
-      #breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
-      #colors <- c("yellow","gold","orange","red")
-      #plot(I_rast, breaks=breakpoints, col=colors, main=tt)
-      #WRITE TO FILE:
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
-      next 
-    }   
-    
     #check if there are any susceptible trees left on the landscape (IF NOT continue LOOP till the end)
-    if(!any(susceptible > 0)){
-      breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
-      colors <- c("yellow","gold","orange","red")
-      plot(I_rast, breaks=breakpoints, col=colors, main=tt)
-      #WRITE TO FILE:
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-      #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
-      break
-    }
+    if(!any(susceptible > 0)) break
     
-    #compute weather suitability coefficients for current time step
+    #is current week time step within a spread month (as defined by input parameters)?
+    if (ss == 'ON' & !any(substr(tt,6,7) %in% months_msk)) next
+    
+    #update counter
+    cnt <- cnt + 1
+          
+    #Total weather suitability:
     W <- as.matrix(Mstack[[cnt]] * Cstack[[cnt]])
     
-    #Within each infected cell (I > 0) draw random number of infections ~Poisson(lambda=rate of spore production) for each infected host. 
-    #Take SUM for total infections produced by each cell. 
-    
-    ### GENERATE SPORES ###:
-    
-    #IF YOU WANT TO USE THE R FUNCTION, USE THIS BLOCK OF CODE  
-    #integer vector
-    #W_lst <- c(t(W))
-    #spores_lst[I_lst > 0] <- mapply(new.infections.gen, x = I_lst[I_lst > 0], rate = 4.4, clim = W_lst[I_lst > 0], SIMPLIFY = TRUE)  #4.4 spores/month from Meentemeyer et al. 2011
-    #integer matrix 
-    #spores_mat <- matrix(spores_lst, ncol=ncol(Nmax_rast), nrow=nrow(Nmax_rast), byrow=T)
-    
-    #IF YOU WANT TO USE THE RCPP FUNCTION, USE THIS BLOCK OF CODE INSTEAD
-    ##using C++ via Rcpp:
+    #GENERATE SPORES:  
     #integer matrix
-    spores_mat <- SporeGen(infected, W, rate = 4.4) 
+    spores_mat <- SporeGenCpp(infected, W, rate = 4.4) 
     
-    ### SPORE DISPERSAL ###:  
+    #SPORE DISPERSAL:  
+    #'List'
+    out <- SporeDispCpp(spores_mat, S=susceptible, I=infected, W, rs=res_win, rtype='Cauchy', wtype='Uniform', scale1=20.57)   #wtype='VM', wdir='N', scale1=20.57, kappa=2)
     
-    #IF YOU WANT TO USE THE R FUNCTION, USE THIS BLOCK OF CODE  
-    ##SporeDisp2 seems faster than SporeDisp, using R alone!!
-    #IF YOU WANT TO USE THE R FUNCTION, USE THIS BLOCK OF CODE 
-    out <- SporeDisp(spores_mat, S=susceptible, I=infected, W, rs=res_win, rtype='Cauchy', scale=20.57, wtype='Uniform')    #wtype='VM', wdir='N', kappa=2  
-    
-    #IF YOU WANT TO USE THE RCPP FUNCTION, USE THIS BLOCK OF CODE INSTEAD
-    ##using C++ via Rcpp:
-    #list output
-    #out <- SporeDispCpp(spores_mat, S=susceptible, I=infected, W, rs=res_win, rtype='Cauchy', wtype='Uniform', scale1=20.57)   #wtype='VM', wdir='N', scale1=20.57, kappa=2)
-    
+    #update R matrices
     susceptible <- out$S 
     infected <- out$I  
     
+    #save matrix into raster obj
     I_rast[] <- infected
-    I_lst <- I_rast[]
-    
-    #IF YOU WANT TO USE THE R FUNCTION, USE THIS BLOCK OF CODE 
-    #wipe out spores vector to use in following time steps
-    #spores_lst <- rep(0, length(Nmax))  #integer
     
     ##CALCULATE OUTPUT TO PLOT:
+    I_lst <- I_rast[]
+    
     # 1) values as % infected
     I_rast[] <- ifelse(I_lst == 0, NA, round(I_lst/Nmax, 1))
     
@@ -226,17 +183,20 @@ for (tt in tstep){
     #I_rast[] <- ifelse(I_lst > 0, 1, 0) 
     #I_rast[] <- ifelse(I_lst > 0, 1, NA) 
     
-    breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
-    colors <- c("yellow","gold","orange","red")
-    plot(I_rast, breaks=breakpoints, col=colors, main=tt)
+    #breakpoints <- c(0, 0.25, 0.5, 0.75, 1)
+    #colors <- c("yellow","gold","orange","red")
+    #plot(I_rast, breaks=breakpoints, col=colors, main=tt)
     
     #WRITE TO FILE:
+    I_rast <- as(I_rast, 'SpatialGridDataFrame')
+    writeRAST(I_rast, vname=paste('Infected_', cnt, '.img', sep=''), overwrite=TRUE) #write to GRASS raster file
+    
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
     #writeRaster(I_rast, filename=paste('./',fOutput,'/Infected_', tt, '.img',sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
   }
   
-  #Sys.sleep(1)
+
 }
 
 
