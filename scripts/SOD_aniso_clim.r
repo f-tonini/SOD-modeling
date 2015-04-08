71ff1f59f7b307959a4da8d6df54736e712495c9
f-tonini <ftonini84@gmail.com>
2015-04-08 11:50:10 -0400

    Modified read init infections & added plot text boxes

--- scripts/SOD_aniso_clim.r
+++ scripts/SOD_aniso_clim.r
@@ -10,7 +10,7 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 #install packages
-#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp", "rgrass7", "optparse"))
+#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp", "rgrass7", "optparse", "plotrix"))
 
 #load packages:
 suppressPackageStartupMessages(library(raster))    #Raster operation and I/O. Depends R (≥ 2.15.0)
@@ -20,6 +20,7 @@ suppressPackageStartupMessages(library(CircStats)) #Circular Statistics - Von Mi
 suppressPackageStartupMessages(library(Rcpp))      #Seamless R and C++ Integration. Depends R (≥ 3.0.0)
 suppressPackageStartupMessages(library(rgrass7))   #Interface Between GRASS 7 GIS and R. Depends R (≥ 2.12)
 suppressPackageStartupMessages(library(optparse))  #Parse args from command line
+suppressPackageStartupMessages(library(plotrix))   #Add text annotations to plot
 
 ##Define the main working directory based on the current script path
 initial_options <- commandArgs(trailingOnly = FALSE)
@@ -42,6 +43,7 @@ sourceCpp("./scripts/myCppFunctions.cpp") #for C++ custom functions
 ###Input simulation parameters: #####
 option_list = list(
   make_option(c("-hi","--host_index"), action="store", default=NA, type='character', help="input host index raster map"),
+  make_option(c("-src","--sources"), action="store", default=NA, type='character', help="initial sources of infection raster map"),
   make_option(c("-img","--image"), action="store", default=NA, type='character', help="background satellite raster image for plotting"),
   make_option(c("-s","--start"), action="store", default=NA, type='integer', help="start year"),
   make_option(c("-e","--end"), action="store", default=NA, type='integer', help="end year"),
@@ -62,12 +64,21 @@ Nmax_rast <- raster(Nmax_rast)  #transform 'sp' obj to 'raster' obj
 #raster resolution
 res_win <- res(Nmax_rast)[1]
 
-#background satellite image for plotting
-<<<<<<< HEAD
-bkr_img <- raster(paste('./layers/', opt$image, sep='')) 
 
-#clone Smax raster to I (=infected trees) raster and spores (=number of spores)
-I_rast <- Nmax_rast 
+##Initial sources of infection:
+I_rast <- readRAST(opt$sources)
+I_rast <- raster(I_rast) #transform 'sp' obj to 'raster' obj
+#I_rast <- raster('./layers/init_cnt.img')
+
+#Susceptibles = Host Index (Nmax) - Infected (I_rast)
+S_rast <- Nmax_rast - I_rast   
+
+#integer matrix with susceptible and infected
+susceptible <- as.matrix(S_rast)
+infected <- as.matrix(I_rast)
+
+##background satellite image for plotting
+bkr_img <- raster(paste('./layers/', opt$image, sep='')) 
 
 ##Start-End date: 
 start <- opt$start
@@ -76,6 +87,17 @@ end <- opt$end
 #end <- arguments[3]
 
 if (start > end) stop('start date must precede end date!!')
+
+#build time series for simulation steps:
+dd_start <- as.POSIXlt(as.Date(paste(start,'-01-01',sep='')))
+dd_end <- as.POSIXlt(as.Date(paste(end,'-12-31',sep='')))
+tstep <- as.character(seq(dd_start, dd_end, 'weeks'))
+
+#create formatting expression for padding zeros depending on total number of steps
+formatting_str = paste("%0", floor( log10( length(tstep) ) ) + 1, "d", sep='')
+#grass date formatting
+months_names = c('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
+
 
 #WEATHER SUITABILITY: read and stack weather suitability raster BEFORE running the simulation
 lst <- dir('./layers/weather', pattern='\\.img$', full.names=T)
@@ -95,46 +117,6 @@ if (ss == 'YES') months_msk <- paste('0', 1:9, sep='') #1=January 9=September
 if (!(opt$wind %in% c('YES', 'NO'))) stop('You must specify whether you want spread by wind or not: use either YES or NO')
 
 
-set.seed(2000)
-
-
-## ----> INITIAL SOURCES OF INFECTION <---------
-#empty vector with counts of I (=infected trees)
-Nmax <- Nmax_rast[]  #Nmax: integer vector from raster
-I_lst <- rep(0, length(Nmax))  #infected: empty integer vector
-
-#how many?
-inf_src <- 2 #integer [0, ncell(Nmax_rast)]
-
-#where?
-#randomly sample the index of cells to become sources
-inf_idx <- sample(which(Nmax > 0), size = inf_src)  
-
-#how many infected host units?
-#randomize the initial I (=infected trees) counts (this does NOT have to exceed Nmax)
-I_lst[inf_idx] <- sapply(Nmax[inf_idx], FUN=function(x) sample(1:x, size=1)) 
-## ------------------------------------------------------------------------------
-
-#Susceptibles = Nmax - Infected 
-S_lst <- Nmax - I_lst   #susceptibles: integer vector
-
-#integer matrix with susceptible and infected
-susceptible <- matrix(S_lst, ncol=ncol(Nmax_rast), nrow=nrow(Nmax_rast), byrow=T)
-infected <- matrix(I_lst, ncol=ncol(Nmax_rast), nrow=nrow(Nmax_rast), byrow=T)
-
-#time counter to access pos index in weather raster stacks
-cnt <- 0 
-
-#build time series for simulation steps:
-dd_start <- as.POSIXlt(as.Date(paste(start,'-01-01',sep='')))
-dd_end <- as.POSIXlt(as.Date(paste(end,'-12-31',sep='')))
-tstep <- as.character(seq(dd_start, dd_end, 'weeks'))
-
-#create formatting expression for padding zeros depending on total number of steps
-formatting_str = paste("%0", floor( log10( length(tstep) ) ) + 1, "d", sep='')
-#grass date formatting
-months_names = c('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
-
 #open window screen
 windows(width = 10, height = 10, xpos=350, ypos=50, buffered = FALSE)
 #quartz()  #use this on Mac OSX
@@ -142,6 +124,13 @@ windows(width = 10, height = 10, xpos=350, ypos=50, buffered = FALSE)
 
 #plot background image
 plot(bkr_img)
+
+#plot coordinates for plotting text:
+xpos <- (bbox(I_rast)[1,2] + bbox(I_rast)[1,1]) / 2
+ypos <- bbox(I_rast)[2,2] - 150
+
+#time counter to access pos index in weather raster stacks
+cnt <- 0 
 
 ## ----> MAIN SIMULATION LOOP (weekly time steps) <------
 for (tt in tstep){
@@ -151,24 +140,24 @@ for (tt in tstep){
   
   if (tt == tstep[1]) {
     
-    if(!any(S_lst > 0)) stop('Simulation ended. There are no more susceptible trees on the landscape!')
+    if(!any(susceptible > 0)) stop('Simulation ended. There are no more susceptible trees on the landscape!')
     
     ##CALCULATE OUTPUT TO PLOT:
     # 1) values as % infected
-    I_rast[] <- ifelse(I_lst == 0, NA, round(I_lst/Nmax, 1))
+    I_rast[] <- ifelse(I_rast[] == 0, NA, round(I_rast[]/Nmax_rast[], 1))
     
     # 2) values as number of infected per cell
-    #I_rast[] <- ifelse(I_lst == 0, NA, I_lst)    
+    #I_rast[] <- ifelse(I_rast[] == 0, NA, I_rast[])
     
     # 3) values as 0 (non infected) and 1 (infected) cell
-    #I_rast[] <- ifelse(I_lst > 0, 1, 0) 
-    #I_rast[] <- ifelse(I_lst > 0, 1, NA) 
+    #I_rast[] <- ifelse(I_rast[] > 0, 1, 0) 
+    #I_rast[] <- ifelse(I_rast[] > 0, 1, NA) 
     
     #PLOT: overlay current plot on background image
     bks <- c(0, 0.25, 0.5, 0.75, 1)
     #colors <- c("yellow","gold","orange","red")
-<<<<<<< HEAD
     plot(I_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=.5)), axes=F, legend=F, add=T)
+    boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
     
     #WRITE TO FILE:
     I_rast_sp <- as(I_rast, 'SpatialGridDataFrame')
@@ -218,17 +207,15 @@ for (tt in tstep){
     I_rast[] <- infected
     
     ##CALCULATE OUTPUT TO PLOT:
-    I_lst <- I_rast[]
-    
     # 1) values as % infected
-    I_rast[] <- ifelse(I_lst == 0, NA, round(I_lst/Nmax, 1))
+    I_rast[] <- ifelse(I_rast[] == 0, NA, round(I_rast[]/Nmax_rast[], 1))
     
     # 2) values as number of infected per cell
-    #I_rast[] <- ifelse(I_lst == 0, NA, I_lst)    
+    #I_rast[] <- ifelse(I_rast[] == 0, NA, I_rast[])
     
     # 3) values as 0 (non infected) and 1 (infected) cell
-    #I_rast[] <- ifelse(I_lst > 0, 1, 0) 
-    #I_rast[] <- ifelse(I_lst > 0, 1, NA) 
+    #I_rast[] <- ifelse(I_rast[] > 0, 1, 0) 
+    #I_rast[] <- ifelse(I_rast[] > 0, 1, NA) 
         
     if (cnt %% opt$nth_output == 0){
       
@@ -236,15 +223,16 @@ for (tt in tstep){
       bks <- c(0, 0.25, 0.5, 0.75, 1)
       #colors <- c("yellow","gold","orange","red")
       plot(I_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=.5)), axes=F, legend=F, add=T)
+      boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
       
       #WRITE TO FILE:
       I_rast_sp <- as(I_rast, 'SpatialGridDataFrame')
       writeRAST(I_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), overwrite=TRUE) #write to GRASS raster file
       execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))  
       
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
+      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
     }
     
   }
