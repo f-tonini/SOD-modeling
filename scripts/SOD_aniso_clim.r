15c815f1d10c32a0d85e0aed4901e9fc6292a20d
f-tonini <ftonini84@gmail.com>
2015-04-29 08:49:41 -0400

    added multihost infection process

--- scripts/SOD_aniso_clim.r
+++ scripts/SOD_aniso_clim.r
@@ -42,7 +42,9 @@ sourceCpp("./scripts/myCppFunctions.cpp") #for C++ custom functions
 
 ###Input simulation parameters: #####
 option_list = list(
-  make_option(c("-hi","--host_index"), action="store", default=NA, type='character', help="input host index raster map"),
+  make_option(c("-u","--umca"), action="store", default=NA, type='character', help="input bay laurel (UMCA) raster map"),
+  make_option(c("-qg","--quag"), action="store", default=NA, type='character', help="input coast live oak (QUAG) raster map"),
+  make_option(c("-qk","--quke"), action="store", default=NA, type='character', help="input black oak (QUKE) raster map"),
   make_option(c("-src","--sources"), action="store", default=NA, type='character', help="initial sources of infection raster map"),
   make_option(c("-img","--image"), action="store", default=NA, type='character', help="background satellite raster image for plotting"),
   make_option(c("-s","--start"), action="store", default=NA, type='integer', help="start year"),
@@ -50,6 +52,7 @@ option_list = list(
   make_option(c("-ss","--seasonal"), action="store", default="YES", type='character', help="seasonal spread?"),
   make_option(c("-w","--wind"), action="store", default="NO", type='character', help="spread using wind?"),
   make_option(c("-pd","--pwdir"), action="store", default=NA, type='character', help="predominant wind direction: N,NE,E,SE,S,SW,W,NW"),
+  make_option(c("-spr","--spore_rate"), action="store", default=4.4, type='numeric', help="spore production rate per week for each infected tree"),
   make_option(c("-o","--output"), action="store", default=NA, type='character', help="basename for output GRASS raster maps"),
   make_option(c("-n","--nth_output"), action="store", default=1, type='integer', help="output every nth map"),
   make_option(c("-scn","--scenario"), action="store", default=NA, type='character', help="future weather scenario")
@@ -57,25 +60,45 @@ option_list = list(
 
 opt = parse_args(OptionParser(option_list=option_list))
 
-##Input raster --> HOST INDEX
-Nmax_rast <- readRAST(opt$host_index)
-Nmax_rast <- raster(Nmax_rast)  #transform 'sp' obj to 'raster' obj
-#Nmax_rast <- raster('./layers/HI_100m.img')
+##Input rasters: abundance (tree density per hectare)
+#----> UMCA
+umca_rast <- readRAST(opt$umca)
+umca_rast <- raster(umca_rast)  #transform 'sp' obj to 'raster' obj
+#----> QUAG
+quag_rast <- readRAST(opt$quag)
+quag_rast <- raster(quag_rast)  
+#----> QUAG
+quke_rast <- readRAST(opt$quke)
+quke_rast <- raster(quke_rast) 
+
+#overall oak host abundance raster
+oaks_rast <- quag_rast + quke_rast
 
 #raster resolution
-res_win <- res(Nmax_rast)[1]
+res_win <- res(umca_rast)[1]
 
-##Initial sources of infection:
-I_rast <- readRAST(opt$sources)
-I_rast <- raster(I_rast) #transform 'sp' obj to 'raster' obj
-#I_rast <- raster('./layers/init_cnt.img')
+##Initial sources of infection (UMCA):
+I_umca_rast <- readRAST(opt$sources)
+I_umca_rast <- raster(I_umca_rast) 
 
-#Susceptibles = Host Index (Nmax) - Infected (I_rast)
-S_rast <- Nmax_rast - I_rast   
+I_quag_rast <- readRAST(opt$sources)
+I_quag_rast <- raster(I_quag_rast)
+
+I_quke_rast <- readRAST(opt$sources)
+I_quke_rast <- raster(I_quke_rast)
+
+#Susceptibles = Current Abundance - Infected (I_rast)
+S_umca_rast <- umca_rast - I_umca_rast   
+S_quag_rast <- quag_rast - I_quag_rast   
+S_quke_rast <- quke_rast - I_quke_rast   
 
 #integer matrix with susceptible and infected
-susceptible <- as.matrix(S_rast)
-infected <- as.matrix(I_rast)
+susceptible_umca <- as.matrix(S_umca_rast)
+infected_umca <- as.matrix(I_umca_rast)
+susceptible_quag <- as.matrix(S_quag_rast)
+infected_quag <- as.matrix(I_quag_rast)
+susceptible_quke <- as.matrix(S_quke_rast)
+infected_quke <- as.matrix(I_quke_rast)
 
 ##background satellite image for plotting
 bkr_img <- raster(paste('./layers/', opt$image, sep='')) 
@@ -191,39 +214,41 @@ for (tt in tstep){
   
   if (tt == tstep[1]) {
     
-    if(!any(susceptible > 0)) stop('Simulation ended. There are no more susceptible trees on the landscape!')
+    if(!any(susceptible_quag > 0) & !any(susceptible_quke > 0)) stop('Simulation ended. There are no more susceptible oaks on the landscape!')
     
     ##CALCULATE OUTPUT TO PLOT:
+    I_oaks_rast <- I_quag_rast + I_quke_rast
+    
     # 1) values as % infected
-    I_rast[] <- ifelse(I_rast[] == 0, NA, round(I_rast[]/Nmax_rast[], 1))
+    I_oaks_rast[] <- ifelse(I_oaks_rast[] == 0, NA, round(I_oaks_rast[]/oaks_rast[], 1))
     
     # 2) values as number of infected per cell
-    #I_rast[] <- ifelse(I_rast[] == 0, NA, I_rast[])
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] == 0, NA, I_oaks_rast[])
     
     # 3) values as 0 (non infected) and 1 (infected) cell
-    #I_rast[] <- ifelse(I_rast[] > 0, 1, 0) 
-    #I_rast[] <- ifelse(I_rast[] > 0, 1, NA) 
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, 0) 
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, NA) 
     
     #PLOT: overlay current plot on background image
     bks <- c(0, 0.25, 0.5, 0.75, 1)
     #colors <- c("yellow","gold","orange","red")
-    image(I_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=1)), add=T, axes=F, box=F, ann=F, legend=F, useRaster=T)
+    image(I_oaks_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=1)), add=T, axes=F, box=F, ann=F, legend=F, useRaster=T)
     boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
     
     #WRITE TO FILE:
-    I_rast_sp <- as(I_rast, 'SpatialGridDataFrame')
-    writeRAST(I_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, 0), sep=''), overwrite=TRUE) #write to GRASS raster file
+    I_oaks_rast_sp <- as(I_oaks_rast, 'SpatialGridDataFrame')
+    writeRAST(I_oaks_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, 0), sep=''), overwrite=TRUE) #write to GRASS raster file
 	  execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))
     
-    #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-    #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-    #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+    #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+    #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
+    #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
     
   }else{
     
     
     #check if there are any susceptible trees left on the landscape (IF NOT continue LOOP till the end)
-    if(!any(susceptible > 0)) break
+    if(!any(susceptible_quag > 0) & !any(susceptible_quke > 0)) break
     
     #update week counter
     cnt <- cnt + 1
@@ -236,7 +261,7 @@ for (tt in tstep){
     
     #GENERATE SPORES:  
     #integer matrix
-    spores_mat <- SporeGenCpp(infected, W, rate = 4.4) 
+    spores_mat <- SporeGenCpp(infected_umca, W, rate = opt$spore_rate) #rate: spores/week for each infected host (4.4 default)
     
     #SPORE DISPERSAL:  
     #'List'
@@ -244,46 +269,59 @@ for (tt in tstep){
       
       #Check if predominant wind direction has been specified correctly:
       if (!(opt$pwdir %in% c('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'))) stop('A predominant wind direction must be specified: N, NE, E, SE, S, SW, W, NW')
-      out <- SporeDispCppWind(spores_mat, S=susceptible, I=infected, W, rs=res_win, rtype='Cauchy', scale1=20.57, wdir=opt$pwdir, kappa=2)
+      out <- SporeDispCppWind_mh(spores_mat, S_UM=susceptible_UM, S_QA=susceptible_QA, S_QK=susceptible_QK, 
+                                I_UM=infected_UM, I_QA=infected_QA, I_QK=infected_QK, W, rs=res_win, rtype='Cauchy', scale1=20.57, wdir=opt$pwdir, kappa=2)
     
     }else{
-      out <- SporeDispCpp(spores_mat, S=susceptible, I=infected, W, rs=res_win, rtype='Cauchy', scale1=20.57)
+      out <- SporeDispCpp_mh(spores_mat, S_UM=susceptible_UM, S_QA=susceptible_QA, S_QK=susceptible_QK, 
+                             I_UM=infected_UM, I_QA=infected_QA, I_QK=infected_QK, W, rs=res_win, rtype='Cauchy', scale1=20.57) ##TO DO
     }  
     
-    #update R matrices
-    susceptible <- out$S 
-    infected <- out$I  
+    #update R matrices:
+    #UMCA
+    susceptible_UM <- out$S_UM 
+    infected_UM <- out$I_UM 
+    #QUAG
+    susceptible_QA <- out$S_QA 
+    infected_QA <- out$I_QA
+    #QUKE
+    susceptible_QK <- out$S_QK 
+    infected_QK <- out$I_QK 
     
     #save matrix into raster obj
-    I_rast[] <- infected
+    I_quag_rast[] <- infected_QA
+    I_quke_rast[] <- infected_QK
     
     ##CALCULATE OUTPUT TO PLOT:
+    I_oaks_rast <- I_quag_rast + I_quke_rast
+    
     # 1) values as % infected
-    I_rast[] <- ifelse(I_rast[] == 0, NA, round(I_rast[]/Nmax_rast[], 1))
+    I_oaks_rast[] <- ifelse(I_oaks_rast[] == 0, NA, round(I_oaks_rast[]/oaks_rast[], 1))
     
     # 2) values as number of infected per cell
-    #I_rast[] <- ifelse(I_rast[] == 0, NA, I_rast[])
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] == 0, NA, I_oaks_rast[])
     
     # 3) values as 0 (non infected) and 1 (infected) cell
-    #I_rast[] <- ifelse(I_rast[] > 0, 1, 0) 
-    #I_rast[] <- ifelse(I_rast[] > 0, 1, NA) 
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, 0) 
+    #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, NA) 
         
     if (cnt %% opt$nth_output == 0){
       
       #PLOT: overlay current plot on background image
       bks <- c(0, 0.25, 0.5, 0.75, 1)
       #colors <- c("yellow","gold","orange","red")
-      image(I_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=.5)), add=T, axes=F, box=F, ann=F, legend=F, useRaster=T)
+      image(I_oaks_rast, breaks=bks, col=rev(heat.colors(length(bks)-1, alpha=.5)), add=T, axes=F, box=F, ann=F, legend=F, useRaster=T)
       boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
       
       #WRITE TO FILE:
-      I_rast_sp <- as(I_rast, 'SpatialGridDataFrame')
-      writeRAST(I_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), overwrite=TRUE) #write to GRASS raster file
-      execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))  
+      I_oaks_rast_sp <- as(I_oaks_rast, 'SpatialGridDataFrame')
+      writeRAST(I_oaks_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, 0), sep=''), overwrite=TRUE) #write to GRASS raster file
+      execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))
       
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-      #writeRaster(I_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+      #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+      #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
+      #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, 0), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+      
     }
     
   }
