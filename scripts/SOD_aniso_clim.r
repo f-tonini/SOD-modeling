da316a509e4f243ff07904eb0bcfdd31d298f6a7
f-tonini <ftonini84@gmail.com>
2015-07-14 11:37:00 -0400

    renamed & formatted for standalone R script

--- scripts/SOD_aniso_clim.r
+++ scripts/SOD_aniso_clim.r
@@ -10,7 +10,7 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 #install packages
-#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp", "rgrass7", "optparse", "plotrix"))
+#install.packages(c("rgdal","raster","lubridate","CircStats","Rcpp", "plotrix"))
 
 #load packages:
 suppressPackageStartupMessages(library(raster))    #Raster operation and I/O. Depends R (≥ 2.15.0)
@@ -18,15 +18,10 @@ suppressPackageStartupMessages(library(rgdal))     #Geospatial data abstraction 
 suppressPackageStartupMessages(library(lubridate)) #Make dealing with dates a little easier. Depends R (≥ 3.0.0)
 suppressPackageStartupMessages(library(CircStats)) #Circular Statistics - Von Mises distribution
 suppressPackageStartupMessages(library(Rcpp))      #Seamless R and C++ Integration. Depends R (≥ 3.0.0)
-suppressPackageStartupMessages(library(rgrass7))   #Interface Between GRASS 7 GIS and R. Depends R (≥ 2.12)
-suppressPackageStartupMessages(library(optparse))  #Parse args from command line
 suppressPackageStartupMessages(library(plotrix))   #Add text annotations to plot
 
 ##Define the main working directory based on the current script path
-initial_options <- commandArgs(trailingOnly = FALSE)
-file_arg <- "--file="
-base_name <- dirname(sub(file_arg, "", initial_options[grep(file_arg, initial_options)]))
-setwd(paste(sep="/", base_name, ".."))
+setwd("D:\\SOD-modeling")
 
 #Path to folders in which you want to save all your vector & raster files
 #fOutput <- 'output'
@@ -40,47 +35,26 @@ setwd(paste(sep="/", base_name, ".."))
 source('./scripts/myfunctions_SOD.r')
 sourceCpp("./scripts/myCppFunctions.cpp") #for C++ custom functions
 
-###Input simulation parameters: #####
-option_list = list(
-  make_option(c("-u","--umca"), action="store", default=NA, type='character', help="input bay laurel (UMCA) raster map"),
-  make_option(c("-ok","--oaks"), action="store", default=NA, type='character', help="input SOD-oaks raster map"),  
-  make_option(c("-lvt","--livetree"), action="store", default=NA, type='character', help="input live tree (all) raster map"),
-  make_option(c("-src","--sources"), action="store", default=NA, type='character', help="initial sources of infection raster map"),
-  make_option(c("-img","--image"), action="store", default=NA, type='character', help="background satellite raster image for plotting"),
-  make_option(c("-s","--start"), action="store", default=NA, type='integer', help="start year"),
-  make_option(c("-e","--end"), action="store", default=NA, type='integer', help="end year"),
-  make_option(c("-ss","--seasonal"), action="store", default="YES", type='character', help="seasonal spread?"),
-  make_option(c("-w","--wind"), action="store", default="NO", type='character', help="spread using wind?"),
-  make_option(c("-pd","--pwdir"), action="store", default=NA, type='character', help="predominant wind direction: N,NE,E,SE,S,SW,W,NW"),
-  make_option(c("-spr","--spore_rate"), action="store", default=4.4, type='numeric', help="spore production rate per week for each infected tree"),
-  make_option(c("-o","--output"), action="store", default=NA, type='character', help="basename for output GRASS raster maps"),
-  make_option(c("-n","--nth_output"), action="store", default=1, type='integer', help="output every nth map"),
-  make_option(c("-scn","--scenario"), action="store", default=NA, type='character', help="future weather scenario")
-)
-
-opt = parse_args(OptionParser(option_list=option_list))
-
 ##Input rasters: abundance (tree density per hectare)
 #----> UMCA
-umca_rast <- readRAST(opt$umca)
-umca_rast <- round(raster(umca_rast))  #transform 'sp' obj to 'raster' obj
+umca_rast <- raster("./layers/UMCA_den_100m.img")
+umca_rast <- round(umca_rast)  #transform 'sp' obj to 'raster' obj
 #----> ALL SOD-affected oaks
-oaks_rast <- readRAST(opt$oaks)
-oaks_rast <- round(raster(oaks_rast))
+oaks_rast <- raster("./layers/OAKS_den_100m.img")
+oaks_rast <- round(oaks_rast)
 #max
 mx <- cellStats(oaks_rast, stat='max') 
 #----> All live trees
-lvtree_rast <- readRAST(opt$livetree)
-lvtree_rast <- round(raster(lvtree_rast))
+lvtree_rast <- raster("./layers/TPH_den_100m.img")
+lvtree_rast <- round(lvtree_rast)
 #calculate trees that are SOD-immune:
-immune_rast <- lvtree_rast - (umca_rast + oaks_rast) 
+IMM_rast <- lvtree_rast - (umca_rast + oaks_rast) 
 
 #raster resolution
 res_win <- res(umca_rast)[1]
 
 ##Initial infection (OAKS):
-I_oaks_rast <- readRAST(opt$sources) 
-I_oaks_rast <- raster(I_oaks_rast) 
+I_oaks_rast <- raster("./layers/init_2000_cnt.img") 
 
 ##Initial sources of infection (UMCA): assumed
 I_umca_rast <- I_oaks_rast * 2
@@ -91,18 +65,18 @@ S_oaks_rast <- oaks_rast - I_oaks_rast
 S_umca_rast <- umca_rast - I_umca_rast
 
 #integer matrix with susceptible and infected
-susceptible_umca <- as.matrix(S_umca_rast)
-infected_umca <- as.matrix(I_umca_rast)
-susceptible_oaks <- as.matrix(S_oaks_rast)
-infected_oaks <- as.matrix(I_oaks_rast)
-immune_matr <- as.matrix(immune_rast)
+S_umca <- as.matrix(S_umca_rast)
+I_umca <- as.matrix(I_umca_rast)
+S_oaks <- as.matrix(S_oaks_rast)
+I_oaks <- as.matrix(I_oaks_rast)
+IMM_matr <- as.matrix(IMM_rast)
 
 ##background satellite image for plotting
-bkr_img <- raster(paste('./layers/', opt$image, sep='')) 
+bkr_img <- raster("./layers/ortho_5m_color.tif") 
 
 ##Start-End date: 
-start <- opt$start
-end <- opt$end
+start <- 2000
+end <- 2007
 
 if (start > end) stop('start date must precede end date!!')
 
@@ -113,8 +87,6 @@ tstep <- as.character(seq(dd_start, dd_end, 'weeks'))
 
 #create formatting expression for padding zeros depending on total number of steps
 formatting_str = paste("%0", floor( log10( length(tstep) ) ) + 1, "d", sep='')
-#grass date formatting
-months_names = c('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
 
 ##WEATHER SUITABILITY: read and stack weather suitability raster BEFORE running the simulation
 #list of ALL weather layers
@@ -134,62 +106,18 @@ weather_rank <- read.table('./layers/weather/WeatherRanked.csv', header = T, str
 ##future weather scenarios: if current simulation year is past the available data, use future climate 
 ##(1) reading GCM projections from another raster stack  ##TODO!!!
 ##(2) using available historical COMPLETE years (1990-2007), ranked by suitability
-
-#CASE 1: no future weather needed
-if(is.na(opt$scenario) & end <= last_yr){
-  Mlst <- grep(paste(as.character(seq(start,end)), collapse="|"), Mlst, value=TRUE)  #use only the raster files matching the years of interest
-  Clst <- grep(paste(as.character(seq(start,end)), collapse="|"), Clst, value=TRUE) #use only the raster files matching the years of interest  
-  Mstack <- stack(Mlst)  
-  Cstack <- stack(Clst) 
-#CASE 2: RANDOM future weather scenario  
-}else if(opt$scenario == 'random'){
-  if(end <= last_yr) stop('you specified a future weather scenario BUT the end year is not a future year!')
-  yrs <- sample(weather_rank[,1], size = end - last_yr, replace = T)
-  Mlst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Mlst, value=TRUE)
-  Mlst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Mlst, value=TRUE)}))
-  Mlst <- c(Mlst_1, Mlst_2)
-  Mstack <- stack(Mlst) 
-  Clst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Clst, value=TRUE)
-  Clst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Clst, value=TRUE)}))
-  Clst <- c(Clst_1, Clst_2)
-  Cstack <- stack(Clst) 
-#CASE 3: UPPER 50% (favorable) future weather scenario
-}else if(opt$scenario == 'favorable'){
-  if(end <= last_yr) stop('you specified a future weather scenario BUT the end year is not a future year!')
-  yrs <- sample(weather_rank[1:9,1], size = end - last_yr, replace = T)
-  Mlst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Mlst, value=TRUE)
-  Mlst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Mlst, value=TRUE)}))
-  Mlst <- c(Mlst_1, Mlst_2)
-  Mstack <- stack(Mlst) 
-  Clst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Clst, value=TRUE)
-  Clst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Clst, value=TRUE)}))
-  Clst <- c(Clst_1, Clst_2)
-  Cstack <- stack(Clst) 
-#CASE 4: LOWER 50% (unfavorable) future weather scenario
-}else{
-  if(end <= last_yr) stop('you specified a future weather scenario BUT the end year is not a future year!')
-  yrs <- sample(weather_rank[10:18, 1], size = end - last_yr, replace = T)
-  Mlst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Mlst, value=TRUE)
-  Mlst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Mlst, value=TRUE)}))
-  Mlst <- c(Mlst_1, Mlst_2)
-  Mstack <- stack(Mlst) 
-  Clst_1 <- grep(paste(as.character(seq(start,end)), collapse="|"), Clst, value=TRUE)
-  Clst_2 <- unlist(lapply(yrs, FUN=function(x){grep(as.character(x), Clst, value=TRUE)}))
-  Clst <- c(Clst_1, Clst_2)
-  Cstack <- stack(Clst) 
-}
+Mstack <- climGen(Mlst, start, end, weather_rank) #M = moisture(precip)
+Cstack <- climGen(Clst, start, end, weather_rank) #C =temperature(tmean)
 
 ##Seasonality: Do you want the spread to be limited to certain months?
-ss <- opt$seasonal   #'YES' or 'NO'
+ss <- 'YES'   #'YES' or 'NO'
 if (ss == 'YES') months_msk <- paste('0', 1:9, sep='') #1=January 9=September
 
 ##Wind: Do you want the spread to be affected by wind?
-if (!(opt$wind %in% c('YES', 'NO'))) stop('You must specify whether you want spread by wind or not: use either YES or NO')
-
-#open window screen
-windows(width = 10, height = 10, xpos = 350, ypos = 50, buffered = FALSE)
-#quartz()  #use this on Mac OSX
-#x11()     #use this on Linux (not tested!)
+wind <- 'YES' #'YES' or 'NO'
+pwdir <- 'NE'
+spore_rate <- 4.4
+nth_output <- 4
 
 #plot background image
 plot(bkr_img, xaxs = "i", yaxs = "i")
@@ -209,7 +137,7 @@ for (tt in tstep){
   
   if (tt == tstep[1]) {
     
-    if(!any(susceptible_oaks > 0)) stop('Simulation ended. All oaks are infected!')
+    if(!any(S_oaks > 0)) stop('Simulation ended. All oaks are infected!')
     
     ##CALCULATE OUTPUT TO PLOT: 
     # 1) values as % infected
@@ -232,57 +160,65 @@ for (tt in tstep){
     boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
     
     #WRITE TO FILE:
-    I_oaks_rast_sp <- as(I_oaks_rast, 'SpatialGridDataFrame')
-    writeRAST(I_oaks_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), overwrite=TRUE) #write to GRASS raster file
-	  execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))
-
     #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
     #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
     #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
     
   }else{
     
-    
     #check if there are any susceptible oaks left on the landscape (IF NOT continue LOOP till the end)
-    if(!any(susceptible_oaks > 0)) break
+    if(!any(S_oaks > 0)) break
     
     #update week counter
     cnt <- cnt + 1
     
     #is current week time step within a spread month (as defined by input parameters)?
-    if (ss == 'YES' & !any(substr(tt,6,7) %in% months_msk)) next
+    if (ss == 'YES' & !any(substr(tt,6,7) %in% months_msk)) {
+      if (cnt == length(tstep) - 1) {
+        #WRITE TO FILE:
+        #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+        #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
+        #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+        I_umca_rast[] <- I_umca
+        I_umca_rast[] <- ifelse(I_umca_rast[] == 0, NA, I_umca_rast[])
+        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
+        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
+        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
+      }
+      next
+    }
           
     #Total weather suitability:
     W <- as.matrix(Mstack[[cnt]] * Cstack[[cnt]])
     
     #GENERATE SPORES:  
     #integer matrix
-    spores_mat <- SporeGenCpp(infected_umca, W, rate = opt$spore_rate) #rate: spores/week for each infected host (4.4 default)
+    spores_mat <- SporeGenCpp(I_umca, W, rate = spore_rate) #rate: spores/week for each infected host (4.4 default)
     
     #SPORE DISPERSAL:  
     #'List'
-    if (opt$wind == 'YES') {
+    if (wind == 'YES') {
       
       #Check if predominant wind direction has been specified correctly:
-      if (!(opt$pwdir %in% c('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'))) stop('A predominant wind direction must be specified: N, NE, E, SE, S, SW, W, NW')
-      out <- SporeDispCppWind_mh(spores_mat, S_UM=susceptible_umca, S_OK=susceptible_oaks, I_UM=infected_umca, I_OK=infected_oaks, IMM=immune_matr, 
-                                 W, rs=res_win, rtype='Cauchy', scale1=20.57, wdir=opt$pwdir, kappa=2)
+      if (!(pwdir %in% c('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'))) stop('A predominant wind direction must be specified: N, NE, E, SE, S, SW, W, NW')
+      out <- SporeDispCppWind_mh(spores_mat, S_UM=S_umca, S_OK=S_oaks, I_UM=I_umca, I_OK=I_oaks, IMM=IMM_matr, 
+                                 W, rs=res_win, rtype='Cauchy', scale1=20.57, wdir=pwdir, kappa=2)
     
     }else{
-      out <- SporeDispCpp_mh(spores_mat, S_UM=susceptible_umca, S_OK=susceptible_oaks, I_UM=infected_umca, I_OK=infected_oaks, IMM=immune_matr,
+      out <- SporeDispCpp_mh(spores_mat, S_UM=S_umca, S_OK=S_oaks, I_UM=I_umca, I_OK=I_oaks, IMM=IMM_matr,
                              W, rs=res_win, rtype='Cauchy', scale1=20.57) ##TO DO
     }  
     
     #update R matrices:
     #UMCA
-    susceptible_umca <- out$S_UM 
-    infected_umca <- out$I_UM 
+    S_umca <- out$S_UM 
+    I_umca <- out$I_UM 
     #oaks
-    susceptible_oaks <- out$S_OK 
-    infected_oaks <- out$I_OK
+    S_oaks <- out$S_OK 
+    I_oaks <- out$I_OK
     
     ##CALCULATE OUTPUT TO PLOT:
-    I_oaks_rast[] <- infected_oaks
+    I_oaks_rast[] <- I_oaks
     
     # 1) values as % infected
     #I_oaks_rast[] <- ifelse(I_oaks_rast[] == 0, NA, I_oaks_rast[]/oaks_rast[])
@@ -294,7 +230,7 @@ for (tt in tstep){
     #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, 0) 
     #I_oaks_rast[] <- ifelse(I_oaks_rast[] > 0, 1, NA) 
         
-    if (cnt %% opt$nth_output == 0){
+    if (cnt %% nth_output == 0){
       
       #PLOT: overlay current plot on background image
       #bks <- c(0, 0.25, 0.5, 0.75, 1)
@@ -305,26 +241,10 @@ for (tt in tstep){
       boxed.labels(xpos, ypos, tt, bg="white", border=NA, font=2)
       
       #WRITE TO FILE:
-      I_oaks_rast_sp <- as(I_oaks_rast, 'SpatialGridDataFrame')
-      writeRAST(I_oaks_rast_sp, vname=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), overwrite=TRUE) #write to GRASS raster file
-      execGRASS('r.timestamp', map=paste(opt$output, '_', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))
-      
       #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
       #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
       #writeRaster(I_oaks_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
       
-      if (cnt == length(tstep) - 1) {
-        #WRITE TO FILE:
-        I_umca_rast[] <- infected_umca
-        I_umca_rast[] <- ifelse(I_umca_rast[] == 0, NA, I_umca_rast[])
-        I_umca_rast_sp <- as(I_umca_rast, 'SpatialGridDataFrame')
-        writeRAST(I_umca_rast_sp, vname=paste(opt$output, '_umca', sprintf(formatting_str, cnt), sep=''), overwrite=TRUE) #write to GRASS raster file
-        execGRASS('r.timestamp', map=paste(opt$output, '_umca', sprintf(formatting_str, cnt), sep=''), date=paste(split_date[3], months_names[as.numeric(split_date[2])], split_date[1]))        
-        
-        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='FLT4S', overwrite=TRUE) # % infected as output
-        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='INT1U', overwrite=TRUE) # nbr. infected hosts as output
-        #writeRaster(I_umca_rast, filename=paste('./', fOutput, '/', opt$output, '_', sprintf(formatting_str, cnt), sep=''), format='HFA', datatype='LOG1S', overwrite=TRUE)  # 0=non infected 1=infected output
-      }
       
     }
     
